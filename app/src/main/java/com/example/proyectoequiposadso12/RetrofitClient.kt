913316cc5401200faaad47015aa37a922089fc71
josedavidlunamolano24-afk <josedavidlunamolano24@gmail.com>
2026-08-01 11:52:36 -0500

    creacion de el object y organizacon de su logica

---
+++ app/src/main/java/com/example/proyectoequiposadso12/RetrofitClient.kt
@@ -0,0 +1,33 @@
+package com.example.proyectoequiposadso12
+
+import android.R.attr.level
+import com.google.firebase.appdistribution.gradle.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.getValue
+
+object RetrofitClient {
+
+    private const val BASE_URL = "https://dummyjson.com/"
+
+    // Muestra en el Logcat lo que se envía y se recibe (útil para depurar)
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    // 'by lazy' = se crea solo la primera vez que se usa
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
